50be88152a5162838b53dac185578a05db8c963f
sanjeevjayasurya <sanjeev11398@gmail.com>
2023-11-26 23:28:38 +0530

    feat: add krishna image and change background and add text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,13 +3,13 @@ import HeroBottom from "./HeroBottomComponent";
 
 export default function Hero() {
   return (
-    <section class="w-screen bg-black flex justify-center items-center flex-col p-10">
+    <section class="w-screen universe-background flex justify-center items-center flex-col p-10">
       <div class="relative inline-flex group">
         <div class="absolute transitiona-all duration-1000 opacity-[200] -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-[40px] blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
         <Link
           href="#"
           title="Get quote now"
-          class="relative inline-flex rounded-[40px] items-center justify-center px-4 py-2 text-lg font-semibold text-black transition-all duration-200 bg-white font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+          class="relative text-center inline-flex rounded-[40px] items-center justify-center px-4 py-2 text-lg font-semibold text-black transition-all duration-200 bg-white font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
           role="button"
         >
           <span>
--- src/components/HeroBottomComponent.js
+++ src/components/HeroBottomComponent.js
@@ -1,12 +1,10 @@
 import { CheckCircleIcon } from "@heroicons/react/20/solid";
 
 const benefits = [
-  "Competitive salaries",
-  "Flexible work hours",
-  "30 days of paid vacation",
-  "Annual team retreats",
-  "Benefits for you and your family",
-  "A great work environment",
+  "Achieve <span class='text-[#F19F0D]'>Inner Peace and Resilience</span>",
+  "Discover the <span class='text-[#F19F0D]'>Mysteries of Existence</span>",
+  "Foster a <span class='text-[#F19F0D]'>Divine Connection</span>",
+  "Harmonize Your <span class='text-[#F19F0D]'>Relationships and Personal Growth</span>",
 ];
 
 export default function HeroBottom() {
@@ -14,41 +12,42 @@ export default function HeroBottom() {
     // <div className="bg-gray-900 py-24 sm:py-32">
     <div className="relative isolate">
       <div className="mx-auto max-w-7xl sm:px-3 lg:px-4">
-        <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-6 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-10 xl:gap-x-20 xl:px-10">
+        <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/20 px-6 py-6 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-10 xl:gap-x-20 xl:px-10">
           <img
             className="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm"
-            src="https://images.unsplash.com/photo-1519338381761-c7523edc1f46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+            src="./krishna_chariot.webp"
             alt=""
           />
-          <div className="w-full flex-auto">
-            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-              Join our team
-            </h2>
-            <p className="mt-6 text-lg leading-8 text-gray-300">
-              Lorem ipsum dolor sit amet consect adipisicing elit. Possimus
-              magnam voluptatum cupiditate veritatis in accusamus quisquam.
+          <div className="w-full flex-auto bg-white p-10 rounded-xl">
+            {/* <h2 className="text-3xl font-bold tracking-tight text-black sm:text-4xl">
+              Subscribe to the ways of vedha
+            </h2> */}
+            <p className="text-xl leading-8 text-black">
+              Join us on a journey to rediscover yourself.
+              <br />
+              The subscription helps you to:
             </p>
             <ul
               role="list"
-              className="mt-10 grid grid-cols-1 gap-x-8 gap-y-3 text-base leading-7 text-white sm:grid-cols-2"
+              className="mt-10 flex flex-col gap-x-8 gap-y-3 text-xl leading-7 text-black sm:grid-cols-2"
             >
               {benefits.map((benefit) => (
-                <li key={benefit} className="flex gap-x-3">
+                <li key={benefit} className="flex gap-x-3 font-semibold">
                   <CheckCircleIcon
                     className="h-7 w-5 flex-none"
                     aria-hidden="true"
                   />
-                  {benefit}
+                  <div dangerouslySetInnerHTML={{ __html: benefit }}></div>
                 </li>
               ))}
             </ul>
             <div className="mt-10 flex">
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-indigo-400"
+              <button
+                type="button"
+                class="flex justify-center items-center max-w-sm w-full bg-gradient-to-r from-indigo-500 via-pink-500 to-yellow-500 hover:from-indigo-600 hover:via-pink-600 hover:to-red-600 focus:outline-none text-white text-2xl uppercase font-bold shadow-md rounded-full mx-auto p-5"
               >
-                See our job postings <span aria-hidden="true">&rarr;</span>
-              </a>
+                <div class="">Subscribe now!</div>
+              </button>
             </div>
           </div>
         </div>
@@ -58,7 +57,7 @@ export default function HeroBottom() {
         aria-hidden="true"
       >
         <div
-          className="aspect-[1318/752] w-[82.375rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-25"
+          className="aspect-[1318/752] w-[82.375rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-50"
           style={{
             clipPath:
               "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
